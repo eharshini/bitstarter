8ff5e2220f8353e863eda6f20a2afa0eead8cec0
eharshini <harshrai94@gmail.com>
2013-07-10 17:32:34 -0400

    third commit

--- web.js
+++ web.js
@@ -2,8 +2,9 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var buffer = fs.readFileSync('/index.html');
-buffer.toString("utf-8")
+app.get('/', function(request, response) {
+response.send(fs.readFileSync('/index.html'));
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
