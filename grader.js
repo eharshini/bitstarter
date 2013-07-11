234776f66bef1a2c3ff96fcbdbc9576e202f98e4
eharshini <harshrai94@gmail.com>
2013-07-11 01:42:49 -0400

    My second grader.js file

--- grader.js
+++ grader.js
@@ -5,7 +5,23 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var sys = require('util');
+var rest = require('restler');
+var URL_DEFAULT = "http://fierce-shelf-7992.herokuapp.com/";
+var assertUrlExists = function (val) {return val.toString();};
 
+var callThis = function(result) {
+if (data instanceof Error) {
+ sys.puts('Error: ' + result.message);
+ this.retry(5000);
+ } else {
+     sys.puts(result);
+ }
+};
+
+var clone = function(fn) {
+ return fn.bind({});
+};
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -35,19 +51,26 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
-var clone = function(fn) {
- return fn.bind({});
-};
 
 if(require.main == module) {
     program
-       .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-       .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+       .option('-c, --checks <check_file>', 'checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+       .option('-f, --file <html_file>', 'index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+       .option('-u, --url <url>', 'url to check', URL_DEFAULT)
        .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+console.log(program.url);
+if(program.url) {
+  rest.get(program.url).on('complete', function(result) {
+     fs.writeFileSync("myfile.html", result);
+    var checkJson = checkHtmlFile("index.html", program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
+  });
+
 } else {
-    exports.checkHtmlFile = checkHtmlFile;
+    var checkJson = checkHtmlFile(result, program.checks);
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
+}
 }
 
